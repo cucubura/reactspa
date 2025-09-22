c93833a23d76352ef327c0b319960c385c9faa5a
cucubura <cucubura@gmail.com>
2025-09-22 18:50:22 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,13 @@
-import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
-import { useEffect } from "react";
-import { pushPageView } from "./utils/analytics";
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import App from './App.jsx'
+import './App.css'
 
-// Import your page components
-import Home from "./pages/Home";
-import About from "./pages/About";
-import Contact from "./pages/Contact";
-import Help from "./pages/Help";
-
-function RouteTracker() {
-  const location = useLocation();
-
-  useEffect(() => {
-    const pageMap = {
-      "/": { name: "Home", category: "General" },
-      "/about": { name: "About Us", category: "Company" },
-      "/contact": { name: "Contact Us", category: "Engagement" },
-      "/help": { name: "Help", category: "Support" }
-    };
-
-    const { name, category } = pageMap[location.pathname] || {
-      name: location.pathname,
-      category: "Other"
-    };
-
-    pushPageView(name, category);
-  }, [location]);
-
-  return null;
-}
-
-export default function App() {
-  return (
-    <Router>
-      <RouteTracker />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/help" element={<Help />} />
-      </Routes>
-    </Router>
-  );
-}
+createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+)
