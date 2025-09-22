7e0a57499f2c0bf3f3b84cbfeab71c4a76d5a40c
sivi <cucubura@gmail.com>
2025-09-22 16:24:36 +0530

    new app file

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 import { useEffect } from "react";
-import { useLocation } from "react-router-dom";
 import { pushPageView } from "./utils/analytics";
 
 // Import your page components
